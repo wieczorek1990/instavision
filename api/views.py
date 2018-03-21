eaa6ec1bf1c6c33169f1c5b6f5cb0dbced31da95
Łukasz Wieczorek <lukasz.wieczorek@uxdream.com>
2018-03-21 10:39:22 +0100

    Add image upload

--- api/views.py
+++ api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import views
+from rest_framework import response
+from rest_framework import status
 
-# Create your views here.
+from api import models
+from api import forms
+from django import http
+
+
+class ImageUpload(views.APIView):
+    @staticmethod
+    def post(request):
+        form = forms.ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = models.Image.objects.create(image=form.cleaned_data['image'])
+            return http.HttpResponse('Upload successful.')
+        else:
+            return response.Response(status=status.HTTP_400_BAD_REQUEST)
